90ee634f6f9f3db749ead569a69d04a22a37e2a0
Maria Kirilova <mariakirilova2004@gmail.com>
2024-03-30 14:00:17 +0200

    model

--- py/pythonProject/app.py
+++ py/pythonProject/app.py
@@ -6,5 +6,3 @@ app= Flask(__name__, static_folder='dist', static_url_path='/')
 def index():
   return send_from_directory(app.static_folder, 'index.html')
 
-if __name__ =='__main__':
-  app.run(); 
