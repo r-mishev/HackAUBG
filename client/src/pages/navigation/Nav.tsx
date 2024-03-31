2d9bed8f502ed0d207aee1a842b7b6fc6146c6b7
Radko Mishev <radko.mishev@evnisoft.com>
2024-03-31 11:46:58 +0300

    changes

--- client/src/pages/navigation/Nav.tsx
+++ client/src/pages/navigation/Nav.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import "./nav.css";
 import { Routes } from "../../utils/routes";
-import Footer from "../footer/Footer";
 
 function Nav() {
   const navigate = useNavigate();
